6ba76da819746543224371110cb05eaf40cf0972
doru87 <32599645+doru87@users.noreply.github.com>
2019-05-09 18:03:00 +0300

    Update CitesteDateConsola.java

--- aplicatie/src/aplicatie/CitesteDateConsola.java
+++ aplicatie/src/aplicatie/CitesteDateConsola.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aplicatie;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- *
- * @author John
- */
 public class CitesteDateConsola {
     
     public static String stringInput() {
